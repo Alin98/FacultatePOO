8c1416c2975afcbcee7d84ded4d4421b514fe5df
Alin98 <38643372+Alin98@users.noreply.github.com>
2019-01-08 02:44:34 +0200

    Add files via upload

---
+++ DisciplineRep.h
@@ -0,0 +1,16 @@
+#ifndef DISCIPLINEREP_H_INCLUDED
+#define DISCIPLINEREP_H_INCLUDED
+#include "Discipline.hpp"
+#include "Repository.hpp"
+#include <vector>
+
+class DisciplineRepository : public Repository<Discipline>
+{
+public:
+   Discipline* findBySubject(std::string name);
+   Discipline* findByType(std::string type);
+
+
+};
+
+#endif // DISCIPLINEREP_H_INCLUDED
